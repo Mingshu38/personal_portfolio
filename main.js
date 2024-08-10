c2007263b8d8d97ae9b637148422a49ba986c513
Joly Sonia <sonia.joly38@gmail.com>
2024-08-10 10:10:35 +0200

    burger menu JS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = document.querySelector("i");
+const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e)=>{
     navLinks.classList.toggle("open")
@@ -8,3 +8,8 @@ menuBtn.addEventListener("click", (e)=>{
     const isOpen = navLinks.classList.contains("open")
     menuBtnIcon.setAttribute("class", isOpen?"ri-close-line":"ri-menu-3-line");
 });
+
+navLinks.addEventListener("click", (e)=>{
+    navLinks.classList.remove("open")
+    menuBtnIcon.setAttribute("class", "ri-menu-3-line")
+})
